2988ffe82b6518df5e0295f3503608b128e037d5
mirabellej <mirabellejones@gmail.com>
2021-08-27 13:54:48 +0200

    Add files via upload

---
+++ Sound.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_CircuitPlayground.h>
+
+// play it only once!
+void setup() {
+  CircuitPlayground.begin();
+  
+  CircuitPlayground.playTone(500, 1000); // (sound in hertz, for how long?)
+  delay(1000); // silence for 1000 milliseconds or one second
+}
+
+// play it over and over
+void loop() {
+
+}
