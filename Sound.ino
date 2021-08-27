2bff972956d5867fb782f24ef806efe359ec15ef
mirabellej <mirabellejones@gmail.com>
2021-08-27 15:36:44 +0200

    Play Sounds!

--- Sound.ino
+++ Sound.ino
@@ -6,6 +6,9 @@ void setup() {
   
   CircuitPlayground.playTone(500, 1000); // (sound in hertz, for how long?)
   delay(1000); // silence for 1000 milliseconds or one second
+  //CircuitPlayground.playTone(200, 1000); // (sound in hertz, for how long?)
+  //delay(1000); // silence for 1000 milliseconds or one second
+  
 }
 
 // play it over and over
